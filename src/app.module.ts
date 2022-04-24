51b99858b49141cb986fe8cd2a57828024494537
RYOTA-F <ry.fj0304@gmail.com>
2022-04-25 01:56:23 +0900

    [add] graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
-import { ItemsModule } from './items/items.module'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql'
+import { DirectiveLocation, GraphQLDirective } from 'graphql'
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive'
+import { RecipesModule } from './recipes/recipes.module'
+
 @Module({
-  imports: [ItemsModule, TypeOrmModule.forRoot(), AuthModule],
+  imports: [
+    RecipesModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+  ],
 })
 export class AppModule {}
